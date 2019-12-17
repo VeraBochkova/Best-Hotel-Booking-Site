4510ccb0bfbf37c8a4c01eb76ea29476b886d94c
Vera <vera.bochkova@mail.ru>
2019-12-17 22:38:50 +0300

    update webpack set less

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,54 @@
 let path = require('path');
+let MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 let conf = {
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js'
+    },
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: 'main.js'
-    }
+        filename: '[name].js',
+        publicPath: './dist'
+    },
+    devServer: {
+        overlay: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader"
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "less-loader",
+                    "style-loader"
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+          filename: '[name].css'
+        })
+    ]
 };
 
-module.exports = conf;
+module.exports = (env, options) => {
+    let production = options.mode === 'production';
+    conf.devtool = production
+                    ? false
+                    : 'evalsourcemap';
+    return conf;
+}
