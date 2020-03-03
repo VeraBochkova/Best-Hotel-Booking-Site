5189943bdf3574acc51efebd91ac5338933844c3
Vera <vera.bochkova@mail.ru>
2020-03-03 11:16:10 +0300

    feat(src/modules): add radio-button-block
    add radio-button-block

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -106,6 +106,7 @@ module.exports = {
     new CopyWebpackPlugin([
       {from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img`},
       {from: `${PATHS.src}/modules/checkbox/Vector`, to: `${PATHS.assets}img`},
+      {from: `${PATHS.src}/modules/radio-button/Rectangle 2.1`, to: `${PATHS.assets}img`},
       {from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts`},
       {from: `${PATHS.src}/${PATHS.assets}static`, to: `${PATHS.assets}static`}
     ]),
